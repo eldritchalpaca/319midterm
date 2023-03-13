3eb425d97ef59f0b296277c53dc30c1125907bcf
zeyoggg <zeyoghan@iastate.edu>
2023-03-12 19:44:08 -0500

    filterByLocation fully operational

--- index.js
+++ index.js
@@ -1,49 +1,108 @@
-fetch('data.json')
-.then(function (response) {
-return response.json();
-})
-.then(function (data) {
-appendData(data);
-})
-.catch(function (err) {
-console.log('error:' + err);
-})
 
-function appendData(data) {
-    let mainContainer = document.getElementById("main_container");
-    let row = document.createElement("div");
-    row.classList.add("row");
-    mainContainer.appendChild(row);
-    let x = 0;
-    for (let name in data) {
-        for (let element of data[name]) {
-            if (x % 3 == 0) {
-                row = document.createElement("div");
-                row.classList.add("row");
-                mainContainer.appendChild(row);
+    fetchJson();
+    let entries = [];
+    let _data;
+    
+    function fetchJson() {
+        fetch('data.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error:' + err);
+            });
+    }
+    
+    function appendData(data) {
+        _data = data;
+        
+        let mainContainer = document.getElementById("main_container");
+        let row = document.createElement("div");
+        row.classList.add("row");
+        mainContainer.appendChild(row);
+        let x = 0;
+        for (let name in data) {
+            for (let element of data[name]) {
+                if (x % 3 == 0) {
+                    row = document.createElement("div");
+                    row.classList.add("row");
+                    mainContainer.appendChild(row);
+                }
+                let col = document.createElement("div");
+                col.classList.add("col");
+                row.appendChild(col);
+    
+                console.log(element);
+                let divName = document.createElement("div");
+                let divDesc = document.createElement("div");
+                let img = document.createElement("img");
+                let link = document.createElement("a");
+                link.href = `${element["imgSource"]}`
+                link.text = "image source"
+                img.src = `${element["image"]}`;
+                img.alt = `${element["name"]}`;
+                img.height = 300;
+                divName.innerText = element["name"];
+                divName.classList.add("title");
+                divDesc.innerHTML = `${element["description"]} <br> ${element["location"]}`;
+                col.appendChild(divName);
+                col.appendChild(img);
+                col.appendChild(divDesc);
+                col.appendChild(link);
+                entries.push(col);
+                x++;
             }
-            let col = document.createElement("div");
-            col.classList.add("col");
-            row.appendChild(col);
+        } // end of for
+    } // end of function appendData
+    
 
-            console.log(element);
-            let div = document.createElement("div");
-            let div2 = document.createElement("div");
-            let img = document.createElement("img");
-            let link = document.createElement("a");
-            link.href = `${element["imgSource"]}`
-            link.text = "image source"
-            img.src = `${element["image"]}`;
-            img.alt = `${element["name"]}`;
-            img.height = 300;
-            div.innerText = element["name"];
-            div.classList.add("title");
-            div2.innerHTML = `${element["description"]} <br>`;
-            col.appendChild(div);
-            col.appendChild(img);
-            col.appendChild(div2);
-            col.appendChild(link);
-            x++;
+    function appendDataButton(arr) {
+        
+        let mainContainer = document.getElementById("main_container");
+        let count = 0;
+        for (let element of entries) {
+            if (arr.length == 0 || arr.includes(count)) {
+                mainContainer.appendChild(element);
+                console.log("here!");
+            }
+            else {
+                console.log("false");
+            }
+            count++;
         }
-    } // end of for
-} // end of function appendData
+    } // end of function appendData
+
+    function filterByLocation() {
+        document.getElementById("main_container").innerHTML = "";
+        document.getElementById("main_container").classList.add("grid");
+
+        let validRegions = [];
+        let display = [];
+
+        if (document.getElementById("asia").checked) {validRegions.push("asia");}
+        if (document.getElementById("europe").checked) {validRegions.push("europe");}
+        if (document.getElementById("NA").checked) {validRegions.push("north america");}
+        if (document.getElementById("global").checked) {validRegions.push("global");}
+        if (validRegions.length == 0) {
+            appendDataButton(display);
+            return;
+        }
+        
+        let found = Object.values(_data.Mushroom).forEach((obj,index) => {
+            for (region of validRegions) {
+                if(obj.location[0] == "global" || obj.location.includes(region)){
+                    display.push(index);
+                }
+            }
+
+        })
+
+
+        appendDataButton(display);
+        
+
+    }
+
