0a3461dc554cd8a32b8716452abd031835dc24cd
zeyoggg <zeyoghan@iastate.edu>
2023-03-12 23:04:06 -0500

    all filters working, minor interface tweaks and sources

--- index.js
+++ index.js
@@ -35,7 +35,6 @@
                 col.classList.add("col");
                 row.appendChild(col);
     
-                console.log(element);
                 let divName = document.createElement("div");
                 let divDesc = document.createElement("div");
                 let img = document.createElement("img");
@@ -47,7 +46,7 @@
                 img.height = 300;
                 divName.innerText = element["name"];
                 divName.classList.add("title");
-                divDesc.innerHTML = `${element["description"]} <br> ${element["location"]}`;
+                divDesc.innerHTML = `${element["description"]} <br> found in ${element["location"]}`;
                 col.appendChild(divName);
                 col.appendChild(img);
                 col.appendChild(divDesc);
@@ -59,23 +58,25 @@
     } // end of function appendData
     
 
-    function appendDataButton(arr) {
+    function appendDataButton(arr, allRegions, allTypes) {
         
+        if (arr.length == 0 && !allRegions && !allTypes) {
+            return;
+        }
+
         let mainContainer = document.getElementById("main_container");
         let count = 0;
+        
         for (let element of entries) {
             if (arr.length == 0 || arr.includes(count)) {
                 mainContainer.appendChild(element);
-                console.log("here!");
-            }
-            else {
-                console.log("false");
             }
             count++;
         }
-    } // end of function appendData
+    }
 
-    function filterByLocation() {
+    function filter() {
+
         document.getElementById("main_container").innerHTML = "";
         document.getElementById("main_container").classList.add("grid");
 
@@ -85,23 +86,72 @@
         if (document.getElementById("asia").checked) {validRegions.push("asia");}
         if (document.getElementById("europe").checked) {validRegions.push("europe");}
         if (document.getElementById("NA").checked) {validRegions.push("north america");}
+        if (document.getElementById("SA").checked) {validRegions.push("south america");}
         if (document.getElementById("global").checked) {validRegions.push("global");}
-        if (validRegions.length == 0) {
-            appendDataButton(display);
-            return;
-        }
-        
-        let found = Object.values(_data.Mushroom).forEach((obj,index) => {
-            for (region of validRegions) {
-                if(obj.location[0] == "global" || obj.location.includes(region)){
+
+
+        allTypes = !document.getElementById("mushroom").checked && !document.getElementById("plant").checked && !document.getElementById("tree").checked;
+        allRegions = validRegions.length == 0;
+        console.log(allTypes);
+        console.log(allRegions);
+        let found;
+
+        let count = 0;
+
+        if (allTypes || document.getElementById("mushroom").checked) {
+            found = Object.values(_data.Mushroom).forEach((obj,index) => {
+                if (allRegions) {
                     display.push(index);
                 }
-            }
+                else {
+                    for (region of validRegions) {
+                        if(obj.location[0] == "global" || obj.location.includes(region)){
+                            display.push(index);
+                        }
+                    }
+                }
+                
+            })
+        }
+        
+        count += _data.Mushroom.length;
+        
+        if (allTypes || document.getElementById("plant").checked) {
+            console.log("in plant");
+            found = Object.values(_data.SmallPlant).forEach((obj,index) => {
+                if (allRegions) {
+                    display.push(index + count);
+                }
+                else {
+                    for (region of validRegions) {
+                        if(obj.location[0] == "global" || obj.location.includes(region)){
+                            console.log("pushing" + obj.name);
+                            display.push(index + count);
+                        }
+                    }
+                }
+            })
+        }
 
-        })
+        count += _data.SmallPlant.length;
 
+        if (allTypes || document.getElementById("tree").checked) {
+            found = Object.values(_data.Tree).forEach((obj,index) => {
+                if (allRegions) {
+                    display.push(index + count);
+                }
+                else {
+                    for (region of validRegions) {
+                        if(allRegions || obj.location[0] == "global" || obj.location.includes(region)){
+                            display.push(index + count);
+                        }
+                    }
+                }
+                
+            })
+        }  
 
-        appendDataButton(display);
+        appendDataButton(display, allRegions, allTypes);
         
 
     }
